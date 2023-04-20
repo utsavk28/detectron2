10f47588f45b88504edcff891c971301ea6c601e
Utsav Khatu <62751545+utsavk28@users.noreply.github.com>
2023-04-20 22:46:15 +0530

    Update events.py

--- detectron2/utils/events.py
+++ detectron2/utils/events.py
@@ -6,7 +6,8 @@ import os
 import time
 from collections import defaultdict
 from contextlib import contextmanager
-from functools import cached_property
+# from functools import cached_property
+from werkzeug.utils import cached_property
 from typing import Optional
 import torch
 from fvcore.common.history_buffer import HistoryBuffer
